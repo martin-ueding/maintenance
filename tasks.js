f65910b6feaa5fb403182419dcda065115628ff1
Martin Ueding <dev@martin-ueding.de>
2013-04-15 09:17:25 +0200

    Convert XML to JSON

---
+++ tasks.js
@@ -0,0 +1,257 @@
+{
+    "backup-chaos": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 1
+    }, 
+    "backup-webservers": {
+        "disk": false, 
+        "internet": true, 
+        "interval": 7
+    }, 
+    "compress-tarball": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 90
+    }, 
+    "copy-korrigiert": {
+        "disk": false, 
+        "internet": true, 
+        "interval": 7
+    }, 
+    "git-autopush": {
+        "disk": true, 
+        "internet": true, 
+        "interval": 1
+    }, 
+    "git-gc": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 30
+    }, 
+    "gnuplot-history": {
+        "disk": false, 
+        "internet": false, 
+        "interval": 1
+    }, 
+    "installed-packages": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 1
+    }, 
+    "legacy-file-formats": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 7
+    }, 
+    "lesshst": {
+        "disk": false, 
+        "internet": false, 
+        "interval": 1
+    }, 
+    "mysql-history": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 1
+    }, 
+    "notes-download": {
+        "disk": false, 
+        "internet": true, 
+        "interval": 1
+    }, 
+    "octave-core-cleanup": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 90
+    }, 
+    "okular-docdata": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 7
+    }, 
+    "php-history": {
+        "disk": false, 
+        "internet": false, 
+        "interval": 1
+    }, 
+    "ppa": {
+        "disk": true, 
+        "internet": true, 
+        "interval": 7
+    }, 
+    "qkey": {
+        "disk": false, 
+        "internet": false, 
+        "interval": 1
+    }, 
+    "thumbnails": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 7
+    }, 
+    "unison-cleanup": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 30
+    }, 
+    "vimrc": {
+        "disk": false, 
+        "internet": true, 
+        "interval": 1
+    }, 
+    "virus-scan-Backup": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 90
+    }, 
+    "virus-scan-Bilder": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 90
+    }, 
+    "virus-scan-Bilder-0000": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-1999": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2001": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2002": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2003": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2004": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2005": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2006": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2007": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2008": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2009": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2010": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2011": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2012": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Bilder-2013": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 90
+    }, 
+    "virus-scan-Branches": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 30
+    }, 
+    "virus-scan-Dokumente": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 30
+    }, 
+    "virus-scan-Downloads": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 7
+    }, 
+    "virus-scan-Installer": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 90
+    }, 
+    "virus-scan-Musik": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 90
+    }, 
+    "virus-scan-PDF_Source": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Packaging": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 30
+    }, 
+    "virus-scan-TODO": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 7
+    }, 
+    "virus-scan-Thunderbird": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 7
+    }, 
+    "virus-scan-VM_Share": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 7
+    }, 
+    "virus-scan-Videos": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 365
+    }, 
+    "virus-scan-Vorlagen": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 30
+    }, 
+    "virus-scan-bin": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 7
+    }, 
+    "virus-scan-dotfiles": {
+        "disk": true, 
+        "internet": false, 
+        "interval": 90
+    }
+}
