97db30bda416dfaf7e51f90ffcbd12e25c89d267
Martin Ueding <dev@martin-ueding.de>
2013-04-26 18:54:24 +0200

    Indent with vim

--- tasks.js
+++ tasks.js
@@ -1,308 +1,308 @@
 {
-    "backup-chaos": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 1,
+	"backup-chaos": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 1,
 		"local": false
-    }, 
-    "backup-webservers": {
-        "disk": false, 
-        "internet": true, 
-        "interval": 7,
+	}, 
+	"backup-webservers": {
+		"disk": false, 
+		"internet": true, 
+		"interval": 7,
 		"local": false
-    }, 
-    "compress-tarball": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 90,
+	}, 
+	"compress-tarball": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 90,
 		"local": false
-    }, 
-    "copy-korrigiert": {
-        "disk": false, 
-        "internet": true, 
-        "interval": 7,
+	}, 
+	"copy-korrigiert": {
+		"disk": false, 
+		"internet": true, 
+		"interval": 7,
 		"local": false
-    }, 
-    "git-autopush": {
-        "disk": true, 
-        "internet": true, 
-        "interval": 1,
+	}, 
+	"git-autopush": {
+		"disk": true, 
+		"internet": true, 
+		"interval": 1,
 		"local": false
-    }, 
-    "git-gc": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 30,
+	}, 
+	"git-gc": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 30,
 		"local": false
-    }, 
-    "gnuplot-history": {
-        "disk": false, 
-        "internet": false, 
-        "interval": 1,
+	}, 
+	"gnuplot-history": {
+		"disk": false, 
+		"internet": false, 
+		"interval": 1,
 		"local": true
-    }, 
-    "goutputstream": {
-        "disk": false, 
-        "internet": false, 
-        "interval": 7,
+	}, 
+	"goutputstream": {
+		"disk": false, 
+		"internet": false, 
+		"interval": 7,
 		"local": true
-    }, 
-    "installed-packages": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 1,
+	}, 
+	"installed-packages": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 1,
 		"local": false
-    }, 
-    "legacy-file-formats": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 7,
+	}, 
+	"legacy-file-formats": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 7,
 		"local": false
-    }, 
-    "lesshst": {
-        "disk": false, 
-        "internet": false, 
-        "interval": 1,
+	}, 
+	"lesshst": {
+		"disk": false, 
+		"internet": false, 
+		"interval": 1,
 		"local": true
-    }, 
-    "mysql-history": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 1,
+	}, 
+	"mysql-history": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 1,
 		"local": true
-    }, 
-    "notes-download": {
-        "disk": false, 
-        "internet": true, 
-        "interval": 1,
+	}, 
+	"notes-download": {
+		"disk": false, 
+		"internet": true, 
+		"interval": 1,
 		"local": false
-    }, 
-    "octave-core-cleanup": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 90,
+	}, 
+	"octave-core-cleanup": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 90,
 		"local": true
-    }, 
-    "okular-docdata": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 7,
+	}, 
+	"okular-docdata": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 7,
 		"local": true
-    }, 
-    "php-history": {
-        "disk": false, 
-        "internet": false, 
-        "interval": 1,
+	}, 
+	"php-history": {
+		"disk": false, 
+		"internet": false, 
+		"interval": 1,
 		"local": true
-    }, 
-    "ppa": {
-        "disk": true, 
-        "internet": true, 
-        "interval": 7,
+	}, 
+	"ppa": {
+		"disk": true, 
+		"internet": true, 
+		"interval": 7,
 		"local": true
-    }, 
-    "qkey": {
-        "disk": false, 
-        "internet": false, 
-        "interval": 1,
+	}, 
+	"qkey": {
+		"disk": false, 
+		"internet": false, 
+		"interval": 1,
 		"local": true
-    }, 
-    "thumbnails": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 7,
+	}, 
+	"thumbnails": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 7,
 		"local": true
-    }, 
-    "unison-cleanup": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 30,
+	}, 
+	"unison-cleanup": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 30,
 		"local": true
-    }, 
-    "vimrc": {
-        "disk": true, 
-        "internet": true, 
-        "interval": 1,
+	}, 
+	"vimrc": {
+		"disk": true, 
+		"internet": true, 
+		"interval": 1,
 		"local": false
-    }, 
-    "virus-scan-Backup": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 90,
+	}, 
+	"virus-scan-Backup": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 90,
 		"local": false
-    }, 
-    "virus-scan-Bilder": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 90,
+	}, 
+	"virus-scan-Bilder": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 90,
 		"local": false
-    }, 
-    "virus-scan-Bilder-0000": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-0000": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-1999": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-1999": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2001": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-2001": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2003": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-2003": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2004": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-2004": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2005": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-2005": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2006": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-2006": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2007": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-2007": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2008": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-2008": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2009": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-2009": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2010": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-2010": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2011": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-2011": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2012": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Bilder-2012": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Bilder-2013": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 90,
+	}, 
+	"virus-scan-Bilder-2013": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 90,
 		"local": false
-    }, 
-    "virus-scan-Branches": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 30,
+	}, 
+	"virus-scan-Branches": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 30,
 		"local": false
-    }, 
-    "virus-scan-Dokumente": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 30,
+	}, 
+	"virus-scan-Dokumente": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 30,
 		"local": false
-    }, 
-    "virus-scan-Downloads": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 7,
+	}, 
+	"virus-scan-Downloads": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 7,
 		"local": false
-    }, 
-    "virus-scan-Installer": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 90,
+	}, 
+	"virus-scan-Installer": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 90,
 		"local": false
-    }, 
-    "virus-scan-Musik": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 90,
+	}, 
+	"virus-scan-Musik": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 90,
 		"local": false
-    }, 
-    "virus-scan-PDF_Source": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-PDF_Source": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Packaging": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 30,
+	}, 
+	"virus-scan-Packaging": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 30,
 		"local": false
-    }, 
-    "virus-scan-TODO": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 7,
+	}, 
+	"virus-scan-TODO": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 7,
 		"local": false
-    }, 
-    "virus-scan-Thunderbird": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 7,
+	}, 
+	"virus-scan-Thunderbird": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 7,
 		"local": false
-    }, 
-    "virus-scan-VM_share": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 7,
+	}, 
+	"virus-scan-VM_share": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 7,
 		"local": false
-    }, 
-    "virus-scan-Videos": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 365,
+	}, 
+	"virus-scan-Videos": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 365,
 		"local": false
-    }, 
-    "virus-scan-Vorlagen": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 30,
+	}, 
+	"virus-scan-Vorlagen": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 30,
 		"local": false
-    }, 
-    "virus-scan-bin": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 7,
+	}, 
+	"virus-scan-bin": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 7,
 		"local": false
-    }, 
-    "virus-scan-dotfiles": {
-        "disk": true, 
-        "internet": false, 
-        "interval": 90,
+	}, 
+	"virus-scan-dotfiles": {
+		"disk": true, 
+		"internet": false, 
+		"interval": 90,
 		"local": false
-    }
+	}
 }
